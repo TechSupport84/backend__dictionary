14fd90d389a7c95d5d98e3468e01aa4e17afc924
jeancy001 <jeancympoy24@gmail.com>
2025-03-18 01:46:05 -0400

    added

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://w-language.netlify.app/",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.get("/set-ad-cookie", (req, res) => {
+  res.cookie("ad_tracking", "enabled", {
+    httpOnly: true,
+    sameSite: "Lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.send("Ad tracking cookie has been set!");
+});
+
+app.get("/get-ad-cookie", (req, res) => {
+  console.log("Cookies received:", req.cookies);
+  const adCookie = req.cookies.ad_tracking || "Not Found";
+  res.send(`Ad Tracking Cookie: ${adCookie}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
